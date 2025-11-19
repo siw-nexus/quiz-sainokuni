e504a7209e0295fa699c36c3439e7904d7be4de2
ogumakouta <siw23010010@gmail.com>
2025-11-19 19:14:23 +0900

    グルメ情報を管理するためのGourmet_spotsモデルを追加
    コード追加：models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -26,3 +26,24 @@ class Tourist_spots(Base):
     img = Column(Text, comment = '画像')
     create_at = Column(DATETIME, server_default=func.now())
     update_at = Column(DATETIME, server_default=func.now(), onupdate=func.now())
+
+
+# gourmet_spotsテーブルのモデル
+class Gourmet_spots(Base):
+    __tablename__ = 'gourmet_spots'
+    
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False, comment = '店舗名')
+    detail = Column(Text, nullable = False, comment = '概要')
+    address = Column(String(255), nullable = False, comment = '住所')
+    lat = Column(String(100), comment = '緯度')
+    lon = Column(String(100), comment = '経度')
+    category = Column(String(100), comment = '区分')
+    tokusanhin = Column(String(255), comment = '特産品')
+    start_time = Column(String(100), comment = '開始時間')
+    finish_time = Column(String(100), comment = '終了時間')
+    notes = Column(Text, comment = '備考')
+    hp_url = Column(Text, comment = 'ホームページURL')
+    img = Column(Text, comment = '画像')
+    create_at = Column(DATETIME, server_default=func.now())
+    update_at = Column(DATETIME, server_default=func.now(), onupdate=func.now())
