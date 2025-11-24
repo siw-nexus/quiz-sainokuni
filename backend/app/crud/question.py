6c6a00a3ee96ea6af366cf765cde7e09e161ca7d
ogumakouta <siw23010010@gmail.com>
2025-11-25 00:59:58 +0900

    データベース接続設定を追加
    コード追加：crud/question.py

---
+++ backend/app/crud/question.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# .envからDATABASE_URLを読み込む
+DATABASE_URL = os.environ['DATABASE_URL']
+
+# データベース接続設定
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"charset": "utf8mb4"}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
