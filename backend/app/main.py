e8b6d34f8a5efbf77bb1cfcbc349336302fa9a85
ogumakouta <siw23010010@gmail.com>
2025-11-26 19:43:34 +0900

    選択肢を取得するエンドポイントを追加
    コード追加：main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,10 @@ from typing import List
 from fastapi import FastAPI, HTTPException, Query
 
 # データベースを操作する関数をインポート
-from app.crud.question import get_question_text
+from app.crud.question import get_question_text, get_options
 
 # レスポンスモデルをインポート
-from app.schemas.question import QestionResponse
+from app.schemas.question import QestionResponse, OptionResponse
 
 
 app = FastAPI()
@@ -22,6 +22,21 @@ def get_questions(
     limit: int = Query(description = "取得する問題数")
 ):
     result = get_question_text(spot_type, limit)
+    
+    # データがからなら404エラーを返す
+    if not result:
+        raise HTTPException(status_code = 404, detail = "データが見つかりませんでした")
+    
+    return result
+
+
+# 選択肢取得
+@app.get("/option", response_model = List[OptionResponse])
+def get_option(
+    spot_type: str = Query(..., description = '観光地(tourist)かグルメ(gourmet)か'),
+    spot_id: int = Query(description = '問題のID')
+):
+    result = get_options(spot_type, spot_id)
     
     # データがからなら404エラーを返す
     if not result:
