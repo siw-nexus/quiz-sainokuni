d3c7e501d5c7e14280cf61369382b34e702d870e
ogumakouta <siw23010010@gmail.com>
2025-11-27 18:44:16 +0900

    正常に選択肢を取得できるかのテストコードを追加
    コードを追加：test_question.py

--- backend/tests/test_question.py
+++ backend/tests/test_question.py
@@ -40,3 +40,44 @@ def test_get_questions_unknown_type():
     # レスポンスの中身の確認
     data = response.json()
     assert data["detail"] == "データが見つかりませんでした"
+
+
+# テストケース：正常に選択肢を取得できるか
+def test_get_options():
+    # spot_typeの指定
+    spot_type = "tourist"
+    # spot_idの指定
+    spot_id = 1
+    
+    # リクエストを送る
+    response = client.get("/option", params={"spot_type": spot_type, "spot_id": spot_id})
+    
+    # ステータスコードの確認
+    assert response.status_code == 200
+
+    # レスポンスの中身の確認
+    data = response.json()
+    assert isinstance(data, list) # リストで返ってくるか
+    assert len(data) == 4         # 選択肢が4件か
+    
+    if len(data) > 0:
+        # 必要なキーが含まれているかチェック
+        assert "id" in data[0]
+        assert "option_text" in data[0]
+        assert "is_correct" in data[0]
+    
+    
+    # 正解と不正解をリストに分ける
+    correct_options = [x for x in data if x["is_correct"] == 1]
+    incorrect_options = [x for x in data if x["is_correct"] == 0]
+
+    # 数を確認
+    assert len(correct_options) == 1
+    assert len(incorrect_options) == 3
+
+    # 「正解」の中身が正しいかチェック
+    assert correct_options[0]["id"] == spot_id
+
+    # 選択肢に重複がないかチェック
+    all_ids = [x["id"] for x in data]
+    assert len(set(all_ids)) == 4
