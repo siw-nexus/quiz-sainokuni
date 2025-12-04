0708b6c3eadacce23b361002cc126776d59a1a85
ogumakouta <siw23010010@gmail.com>
2025-12-04 20:44:26 +0900

    データベース接続設定をmain.pyから受け取って、INSERTした結果を返すように変更
    コード編集：crud/interest.py

--- backend/app/crud/interest.py
+++ backend/app/crud/interest.py
@@ -3,38 +3,32 @@ from sqlalchemy import select
 # モデルをインポート
 from app.models import Interests
 
-# データベース接続設定をインポート
-from app.database import db_connect
-
-
-# セッションの作成
-SessionLocal = db_connect()
-
 
 # 興味あるを保存する関数
-def add_interests(user_id: int, spot_type: str, spot_id: int):
-    with SessionLocal() as db:
-        # データがすでにあるか確認
-        check_interests = select(Interests).where(Interests.user_id == user_id, Interests.spot_type == spot_type, Interests.spot_id == spot_id)
-        
-        # SQL実行
-        check_result = db.execute(check_interests)
-        
-        if check_result.first():
-            # すでにデータがあったら処理終了
-            return
-         
-        # 興味があるを保存
-        add_interests = Interests(
-            user_id = user_id,
-            spot_type = spot_type,
-            spot_id = spot_id
-        )
-        
-        # 保存SQL実行
-        db.add(add_interests)
-        
-        # トランザクションを確定
-        db.commit()
-        
-        return
+def add_interests(db, user_id: int, spot_type: str, spot_id: int):
+    # データがすでにあるか確認
+    stmt = select(Interests).where(Interests.user_id == user_id, Interests.spot_type == spot_type, Interests.spot_id == spot_id)
+    
+    # SQL実行
+    check_result = db.scalars(stmt).first()
+    
+    if check_result:
+        # すでにデータがあったらそれを返す
+        return check_result
+    
+    # 興味があるを保存
+    new_interests = Interests(
+        user_id = user_id,
+        spot_type = spot_type,
+        spot_id = spot_id
+    )
+    
+    # 保存SQL実行
+    db.add(new_interests)
+    
+    # トランザクションを確定
+    db.commit()
+    
+    db.refresh(new_interests)
+    
+    return new_interests
