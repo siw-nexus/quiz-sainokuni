03338fa01d8039fe573e30ced91a90a49ddf3cd2
ogumakouta <siw23010010@gmail.com>
2025-12-04 18:48:56 +0900

    興味があるをデータベースに保存する関数に重複チェックを追加
    コード追加：crud/interest.py

--- backend/app/crud/interest.py
+++ backend/app/crud/interest.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select
 
 # モデルをインポート
 from app.models import Interests
@@ -14,15 +14,27 @@ SessionLocal = db_connect()
 # 興味あるを保存する関数
 def add_interests(user_id: int, spot_type: str, spot_id: int):
     with SessionLocal() as db:
-            # 興味があるを保存
-            add_interests = Interests(
-                user_id = user_id,
-                spot_type = spot_type,
-                spot_id = spot_id
-            )
-            
-            # # valuesを辞書型で定義
-            db.add(add_interests)
-            
-            # トランザクションを確定
-            db.commit()
+        # データがすでにあるか確認
+        check_interests = select(Interests).where(Interests.user_id == user_id, Interests.spot_type == spot_type, Interests.spot_id == spot_id)
+        
+        # SQL実行
+        check_result = db.execute(check_interests)
+        
+        if check_result.first():
+            # すでにデータがあったら処理終了
+            return
+         
+        # 興味があるを保存
+        add_interests = Interests(
+            user_id = user_id,
+            spot_type = spot_type,
+            spot_id = spot_id
+        )
+        
+        # 保存SQL実行
+        db.add(add_interests)
+        
+        # トランザクションを確定
+        db.commit()
+        
+        return
