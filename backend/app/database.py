30b1a5af5c6dcb6d7d5b8d36696476a8e49e523a
ogumakouta <siw23010010@gmail.com>
2025-12-04 20:39:28 +0900

    データベース接続設定をグローバル変数に変更
    コード編集：database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,14 +3,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+# .envからDATABASE_URLを読み込む
+DATABASE_URL = os.environ['DATABASE_URL']
+
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"charset": "utf8mb4"}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 def db_connect():
-    # .envからDATABASE_URLを読み込む
-    DATABASE_URL = os.environ['DATABASE_URL']
-
-
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"charset": "utf8mb4"}
-    )
+    db = SessionLocal()
     
-    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    try:
+        yield db
+    finally:
+        db.close()
