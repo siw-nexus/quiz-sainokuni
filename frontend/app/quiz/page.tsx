cc6cdda182a9b0b7e03817110855439571750edd
ogumakouta <siw23010010@gmail.com>
2025-12-08 00:51:41 +0900

    パラメーターを取得してQuizScreenコンポーネントに渡すコード以外を削除
    コード編集：quiz/page.tsx

--- frontend/app/quiz/page.tsx
+++ frontend/app/quiz/page.tsx
@@ -1,6 +1,5 @@
 // コンポーネントのインポート
-import QuestionText from "@/components/quiz/QuestionText";
-import OptionBtn from "@/components/quiz/OptionBtn";
+import QuizScreen from "@/components/quiz/QuizScreen";
 
 type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -8,33 +7,15 @@ type Props = {
 
 
 export default async function Quiz({ searchParams }: Props) {
-  // APIのエンドポイント
-  const apiUrl = process.env.INTERNAL_API_URL
-
   // URLのパラメーターを取得
   const params = await searchParams;
   const spot_type = params.spot_type; // spot_typeの値を取得
   const limit = params.limit;         // limitの値を取得
 
-  // 問題を取得する
-  const res = await fetch(
-    `${apiUrl}/question?spot_type=${spot_type}&limit=${limit}`,
-    { cache: "no-cache"} // キャッシュを無効化
-  )
-
-  // レスポンスの確認
-  if (!res.ok) {
-    throw new Error("問題の取得に失敗しました");
-  }
-
-  // レスポンスの中身を取得
-  const questions = await res.json();
-
   
   return (
     <main>
-      <QuestionText questions={questions} />
-      <OptionBtn questions={questions} spot_type={spot_type}/>
+      <QuizScreen spot_type={spot_type} limit={limit}/>
     </main>
   );
 }
