89dfe5acd0a60051f314e0bf0a503a2ee82d30dc
ogumakouta <siw23010010@gmail.com>
2025-12-08 01:30:49 +0900

    回答したら結果を表示するコード、全部出題したらfinish画面に移動するコードを追加
    コード追加：QuizScreen.tsx

--- frontend/components/quiz/QuizScreen.tsx
+++ frontend/components/quiz/QuizScreen.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 
 // コンポーネントのインポート
 import QuestionText from "./QuestionText";
@@ -22,8 +23,10 @@ type Props = {
 
 export default function QuizScreen({ spot_type, limit, onResult }: Props) {
   const [questions, setQuestions] = useState<Question[]>([]); // 問題文を格納
-  const [questionCount, setQuestionCount] = useState(1);
+  const [questionCount, setQuestionCount] = useState(1);      // 現在何問目かをカウントする変数
   const [isResponding, setIsResponding] = useState(true);     // 回答中かどうかのフラグ
+  const [isCorrectText, setIsCorrectText] = useState('')      // 「正解」か「不正解」の文字列を格納
+  const router = useRouter();
 
   // APIのエンドポイント
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
@@ -53,30 +56,49 @@ export default function QuizScreen({ spot_type, limit, onResult }: Props) {
 
   // OptionBtnコンポーネントから正誤判定の結果を受け取る
   const handleAnswerResult = (result: boolean) => {
+    // 回答中のフラグをfalseにする
     setIsResponding(false);
-    if (result === true) {
-      console.log("正解");
+    
+    if (result) {
+      setIsCorrectText("正解");
     } else {
-      console.log("残念");
+      setIsCorrectText("不正解");
     }
-
-    // 次の問題
-    setQuestionCount(questionCount + 1)
   };
 
+  // 次の問題へ行く関数
+  const handleNextQuiz = () => {
+    // 次か何問目か
+    const nextCount = questionCount + 1;
+
+    // 次の問題へ
+    setQuestionCount(questionCount + 1)
+    
+    // 出題数が取得した問題数を超えたらfinish画面に移動
+    if (questions.length < nextCount) {
+      router.push('/finish')
+    } else {
+      // 出題数が取得した問題数を超えていなかったら回答中のフラグをtrueにする
+      setIsResponding(true)
+    }
+  };
+
+  // 回答中フラグがtrueだったら問題と選択肢を表示
   if (isResponding) {
     return (
       <main>
+        <p>{questionCount}問目</p>
         <QuestionText questions={questions} questionCount={questionCount}/>
         <OptionBtn questions={questions} spot_type={spot_type} questionCount={questionCount} onResult={handleAnswerResult}/>
       </main>
     );
   } else {
+    // 回答中フラグがfalseだったら答えを表示
     return (
-      <p>結果</p>
+      <main>
+        <p>{isCorrectText}</p>
+        <button onClick={() => handleNextQuiz()}>次の問題へ</button>
+      </main>
     );
   }
-
-
-  return null;
 }
