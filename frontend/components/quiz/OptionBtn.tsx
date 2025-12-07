fe7fc65555e1cfef91e6c84d1516784dacbf0ada
ogumakouta <siw23010010@gmail.com>
2025-12-07 23:12:28 +0900

    選択肢を取得して表示するコードを追加
    コード追加：OptionBtn.tsx

--- frontend/components/quiz/OptionBtn.tsx
+++ frontend/components/quiz/OptionBtn.tsx
@@ -1,17 +1,77 @@
 'use client';
 
-const OPTION_LABELS = {1: 'ア', 2: 'イ', 3: 'ウ', 4: 'エ'}
+import { useState, useEffect } from 'react';
 
-export default function OptionBtn() {
+// questionsの型を定義
+type Question = {
+  id: number;
+  spot_type: 'tourist' | 'gourmet';
+  spot_id: number;
+  question_text: string;
+}
+
+// 選択肢の型を定義
+type Option = {
+  id: number;
+  option_text: string;
+  is_correct: number;
+}
+
+type Props = {
+  questions: Question[];
+  spot_type: 'tourist' | 'gourmet';
+}
+
+// 選択肢を取得する関数
+const getOptions = async (apiUrl: string, spot_type: 'tourist' | 'gourmet', spot_id: number): Promise<Option[]> => {
+  console.log(`${apiUrl}/option?spot_type=${spot_type}&spot_id=${spot_id}`);
+  // 選択肢を取得する
+  const res = await fetch(
+    `${apiUrl}/option?spot_type=${spot_type}&spot_id=${spot_id}`,
+    { cache: "no-cache"} // キャッシュを無効化
+  );
+
+  // レスポンスの確認
+  if (!res.ok) {
+    throw new Error("選択肢の取得に失敗しました");
+  }
+
+  const options = await res.json();
+
+  return options;
+}
+
+
+export default function OptionBtn({ questions, spot_type }: Props) {
+  const [options, setOptions] = useState<Option[]>([]);
+
+  // APIのエンドポイント
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      try {
+        // questions[0]が存在するかチェック
+        if (questions && questions.length > 0) {
+            const data = await getOptions(apiUrl, spot_type, questions[0].id);
+            setOptions(data);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchOptions();
+  }, [apiUrl, spot_type, questions]);
+
   return (
     <div>
-      {Object.entries(OPTION_LABELS).map(([key, label]) => (
+      {options.map((option) => (
         <button
-          key={key}
-          // onClick={() => handleClick(key)}
+          key={option.id}
           className='border'
         >
-          {label}
+          {option.option_text}
         </button>
       ))}
     </div>
