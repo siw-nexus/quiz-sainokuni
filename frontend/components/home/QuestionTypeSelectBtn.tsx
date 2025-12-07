b4c4cc692853096168b59388db3745461ee626b6
ogumakouta <siw23010010@gmail.com>
2025-12-07 18:04:53 +0900

    ボタンを押したらクイズ画面に遷移するコードを追加
    コード追加：QuestionTypeSelectBtn

--- frontend/components/home/QuestionTypeSelectBtn.tsx
+++ frontend/components/home/QuestionTypeSelectBtn.tsx
@@ -1,18 +1,26 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 
 const QUESTION_TYPES = {'tourist': '観光地', 'gourmet': 'グルメ'};
 
 export default function QuestionTypeSelectBtn() {
     const [type, setType] = useState('tourist');
+    const router = useRouter();
+
+    // クイズ画面に遷移する関数
+    const handleClick = (key: string) => {
+      setType(key)
+      router.push(`/quiz?spot_type=${key}`)
+    }
   
     return (
         <div>
         {Object.entries(QUESTION_TYPES).map(([key, label]) => (
           <button
             key={key}
-            onClick={() => setType(key)}
+            onClick={() => handleClick(key)}
             className='border'
           >
             {label}
