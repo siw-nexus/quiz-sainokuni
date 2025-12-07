f71c9d0c757029f4ba4eaa8417514414b1ac2e3b
ogumakouta <siw23010010@gmail.com>
2025-12-07 18:21:06 +0900

    クイズ画面に必要な要素を追加
    コード追加：quiz/page.tsx

---
+++ frontend/app/quiz/page.tsx
@@ -0,0 +1,11 @@
+// コンポーネントのインポート
+import OptionBtn from "@/components/quiz/OptionBtn";
+
+export default function Quiz() {
+  return (
+    <main>
+      <p>問題文</p>
+      <OptionBtn />
+    </main>
+  );
+}
