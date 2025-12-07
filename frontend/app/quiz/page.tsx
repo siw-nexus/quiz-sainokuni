3425c94b95bb27c0b8870dd3acddba941043f829
ogumakouta <siw23010010@gmail.com>
2025-12-07 22:02:46 +0900

    問題を取得するコードを追加
    コード追加：quiz/page.tsx

--- frontend/app/quiz/page.tsx
+++ frontend/app/quiz/page.tsx
@@ -1,7 +1,35 @@
 // コンポーネントのインポート
 import OptionBtn from "@/components/quiz/OptionBtn";
 
-export default function Quiz() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+
+export default async function Quiz({ searchParams }: Props) {
+  // APIのエンドポイント
+  const apiUrl = process.env.INTERNAL_API_URL
+
+  // URLのパラメーターを取得
+  const params = await searchParams;
+  const spot_type = params.spot_type; // spot_typeの値を取得
+  const limit = params.limit;         // limitの値を取得
+
+  // 問題を取得する
+  const res = await fetch(
+    `${apiUrl}/question?spot_type=${spot_type}&limit=${limit}`,
+    { cache: "no-cache"} // キャッシュを無効化
+  )
+
+  // レスポンスの確認
+  if (!res.ok) {
+    throw new Error("問題の取得に失敗しました");
+  }
+
+  // レスポンスの中身を取得
+  const questions = await res.json();
+
+  
   return (
     <main>
       <p>問題文</p>
