0e66c0e9bc01d69456596fa944e4499b48185ac5
shiori42 <siw23010031@class.siw.ac.jp>
2025-12-09 15:05:00 +0900

    cssを追加
    コード追加：page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,8 @@
-import Image from "next/image";
-
-// コンポーネントのインポート
 import QuizSelect from "@/components/home/QuizSelect";
 
 export default function Home() {
   return (
     <main>
-      <button className="border">説明</button>
-      <button><Image src="/human.svg" width={50} height={50} alt="人のアイコン"></Image></button>
-      <h1>クイズ彩の国</h1>
       <QuizSelect />
     </main>
   );
