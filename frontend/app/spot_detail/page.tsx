b3d7af95ccaa3a456853f42a8dbefe846b2eec09
ogumakouta <siw23010010@gmail.com>
2025-12-12 23:08:37 +0900

    URLのパラメーターを取得するコードを追加
    コード追加：spot_detail/page.tsx

--- frontend/app/spot_detail/page.tsx
+++ frontend/app/spot_detail/page.tsx
@@ -1,8 +1,18 @@
 // コンポーネントをインポート
 import Detail from "@/components/Spot_detail/Detail";
 
-export default function SpotDetail() {
-    return (
-        <Detail />
-    );
+// Propsを定義
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function SpotDetail({ searchParams }: Props) {
+  // URLのパラメーターを取得
+  const params = await searchParams;
+  const spot_type = params.spot_type; // spot_typeの値を取得
+  const spot_id = params.spot_id;     // spot_idの値を取得
+
+  return (
+    <Detail />
+  );
 }
