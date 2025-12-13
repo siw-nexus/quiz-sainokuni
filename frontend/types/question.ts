25a7bc7611eda42b42ba1632741dd7fa36368ece
ogumakouta <siw23010010@gmail.com>
2025-12-13 19:20:23 +0900

    問題の型と選択肢の型を合体
    コード修正：question.ts

--- frontend/types/question.ts
+++ frontend/types/question.ts
@@ -1,28 +1,15 @@
+// 選択肢の型を定義
+type Option = {
+  id: number;
+  option_text: string;
+  is_correct: boolean;
+}
+
 // 問題の型を定義
 type Question = {
   id: number;
   spot_type: 'tourist' | 'gourmet';
   spot_id: number;
   question_text: string;
-}
-
-// 選択肢の型を定義
-type Option = {
-  id: number;
-  option_text: string;
-  is_correct: number;
-  detail: string | null;
-  address: string | null;
-  lat: string | null;
-  lon: string | null;
-  availavle_time: string | null;
-  closure_info: string | null;
-  category: string | null;
-  tokusanhin: string | null;
-  start_time: string | null;
-  finish_time: string | null;
-  notes: string | null;
-  tel: string | null;
-  hp_url: string | null;
-  img: string | null;
+  options: Option[];
 }
