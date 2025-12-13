8a5ff275b107adda749b100ae61ad04b82a8ee3e
ogumakouta <siw23010010@gmail.com>
2025-12-14 00:07:35 +0900

    型の定義にexportを追加。 OptionとOptionOnlyの内容が重複してるのでOptionOnlyを削除
    コード修正：question.py

--- frontend/types/question.ts
+++ frontend/types/question.ts
@@ -1,22 +1,15 @@
 // 選択肢の型を定義
-type Option = {
+export type Option = {
   id: number;
   option_text: string;
   is_correct: boolean;
 }
 
 // 問題の型を定義
-type Question = {
+export type Question = {
   id: number;
   spot_type: 'tourist' | 'gourmet';
   spot_id: number;
   question_text: string;
   options: Option[];
-}
-
-// 選択肢だけを受け取る型の定義
-type OptionOnly = {
-  id: number;
-  option_text: string;
-  is_correct: boolean;
 }
