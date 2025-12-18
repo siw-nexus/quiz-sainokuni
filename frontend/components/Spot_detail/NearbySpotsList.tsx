4e3427134624caf3c6d32d0bf371ff05a682f653
ogumakouta <siw23010010@gmail.com>
2025-12-18 17:39:09 +0900

    spot_detail/page.tsxで取得した周辺のスポットをPropsで受け取るコードを追加
    コード追加：NearbySpotsList.tsx

---
+++ frontend/components/Spot_detail/NearbySpotsList.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+// 型の定義をインポート
+import { NearbySpot } from "@/types/spot";
+
+// Propsを定義
+type Props = {
+  nearbySpots: NearbySpot[]
+}
+
+export default function NearbySpotsList({ nearbySpots }: Props) {
+  return null;
+}
