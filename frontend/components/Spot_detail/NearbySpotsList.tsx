d65e4c1434da1fbec8e6d2cb76985d4c586c70a6
ogumakouta <siw23010010@gmail.com>
2025-12-18 18:21:56 +0900

    取得した周辺のスポットを観光地とグルメに分けて表示するコードを追加
    コード追加：NearbySpotsList.tsx

--- frontend/components/Spot_detail/NearbySpotsList.tsx
+++ frontend/components/Spot_detail/NearbySpotsList.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from 'next/link';
+
 // 型の定義をインポート
 import { NearbySpot } from "@/types/spot";
 
@@ -8,6 +10,43 @@ type Props = {
   nearbySpots: NearbySpot[]
 }
 
+const roundWithScale = (value: number, scale: number) => {
+  return Math.round(value * 10 ** scale) / 10 ** scale;
+};
+
 export default function NearbySpotsList({ nearbySpots }: Props) {
-  return null;
+  return (
+    <div>
+      周辺の観光地
+      {nearbySpots
+        .filter((nearbySpot) => nearbySpot.spot_type === 'tourist')
+        .map((nearbySpot) => (
+          <div key={nearbySpot.id}>
+            <p>{nearbySpot.name}</p>
+            <p>約{nearbySpot.distance >= 1 ? `${roundWithScale(nearbySpot.distance, 1)}km` : `${Math.round(nearbySpot.distance * 1000)}m`}</p>
+            <Link
+              href={`/spot_detail?spot_type=${nearbySpot.spot_type}&spot_id=${nearbySpot.id}`}
+              className='border'
+            >
+              詳細を見る
+            </Link>
+          </div>
+      ))}
+      周辺のグルメ
+      {nearbySpots
+        .filter((nearbySpot) => nearbySpot.spot_type === 'gourmet')
+        .map((nearbySpot) => (
+          <div key={nearbySpot.id}>
+            <p>{nearbySpot.name}</p>
+            <p>約{nearbySpot.distance >= 1 ? `${roundWithScale(nearbySpot.distance, 1)}km` : `${Math.round(nearbySpot.distance * 1000)}m`}</p>
+            <Link
+              href={`/spot_detail?spot_type=${nearbySpot.spot_type}&spot_id=${nearbySpot.id}`}
+              className='border'
+            >
+              詳細を見る
+            </Link>
+          </div>
+      ))}
+    </div>
+  );
 }
