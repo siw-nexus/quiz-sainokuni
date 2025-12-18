3a581f328ccea57ca760dbd282877d9091519c81
ogumakouta <siw23010010@gmail.com>
2025-12-18 16:46:21 +0900

    周辺のスポットを取得の観光地とグルメをunion_allで繋げるように変更
    コード修正：crud/spot.py

--- backend/app/crud/spot.py
+++ backend/app/crud/spot.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func, literal
+from sqlalchemy import select, func, literal, union_all
 
 # モデルをインポート
 from app.models import Tourist_spots, Gourmet_spots
@@ -29,7 +29,7 @@ def get_spot(db, spot_type: str, id: int):
 
 
 # 周辺のスポットを取得（2km）
-def get_nearby_spots(db, spot_type: str, current_lat: float, current_lon: float):
+def get_nearby_spots(db, current_lat: float, current_lon: float):
     # 地球の半径（km）
     EARTH_RADIUS = 6371
     
@@ -37,65 +37,66 @@ def get_nearby_spots(db, spot_type: str, current_lat: float, current_lon: float)
     limit_km = 3.0
     
     # 観光地の周辺のスポットを取得
-    if spot_type == 'tourist':
-        # 距離計算のSQL式（ラジアンに変換して計算。球面三角法（余弦定理）っていう計算らしい）
-        # (6371 * acos(cos(lat1) * cos(lat2) * cos(lon2 - lon1) + sin(lat1) * sin(lat2)))
-        distance_expr = (
-            EARTH_RADIUS * func.acos(
-                func.cos(func.radians(current_lat)) *
-                func.cos(func.radians(Tourist_spots.lat)) *
-                func.cos(func.radians(Tourist_spots.lon) - func.radians(current_lon)) +
-                func.sin(func.radians(current_lat)) *
-                func.sin(func.radians(Tourist_spots.lat))
-            )
+    # 距離計算のSQL式（ラジアンに変換して計算。球面三角法（余弦定理）っていう計算らしい）
+    # (6371 * acos(cos(lat1) * cos(lat2) * cos(lon2 - lon1) + sin(lat1) * sin(lat2)))
+    distance_expr = (
+        EARTH_RADIUS * func.acos(
+            func.cos(func.radians(current_lat)) *
+            func.cos(func.radians(Tourist_spots.lat)) *
+            func.cos(func.radians(Tourist_spots.lon) - func.radians(current_lon)) +
+            func.sin(func.radians(current_lat)) *
+            func.sin(func.radians(Tourist_spots.lat))
         )
-        
-        # クエリ定義
-        tourist_near_spots = (
-            select(
-                Tourist_spots.id, 
-                literal(spot_type).label('spot_type'),
-                Tourist_spots.name, 
-                distance_expr.label('distance')
-            ).where(
-                distance_expr <= limit_km,  # 3km以内
-                distance_expr > 0
-            ).order_by(
-                distance_expr.asc() # 近い順
-            ).limit(3)
-        )
-        
-        # 観光地の周辺のスポットを取得して返す
-        return db.execute(tourist_near_spots).mappings().all()
+    )
+    
+    # クエリ定義
+    tourist_near_spots = (
+        select(
+            Tourist_spots.id, 
+            literal('tourist').label('spot_type'),
+            Tourist_spots.name, 
+            distance_expr.label('distance')
+        ).where(
+            distance_expr <= limit_km,  # 3km以内
+            distance_expr > 0
+        ).order_by(
+            distance_expr.asc() # 近い順
+        ).limit(3)
+    )
+    
+    # 観光地の周辺のスポットを取得して返す
+    # return db.execute(tourist_near_spots).mappings().all()
     
     # グルメの周辺のスポットを取得
-    if spot_type == 'gourmet':
-        # 距離計算のSQL式（ラジアンに変換して計算。球面三角法（余弦定理）っていう計算らしい）
-        # (6371 * acos(cos(lat1) * cos(lat2) * cos(lon2 - lon1) + sin(lat1) * sin(lat2)))
-        distance_expr = (
-            EARTH_RADIUS * func.acos(
-                func.cos(func.radians(current_lat)) *
-                func.cos(func.radians(Gourmet_spots.lat)) *
-                func.cos(func.radians(Gourmet_spots.lon) - func.radians(current_lon)) +
-                func.sin(func.radians(current_lat)) *
-                func.sin(func.radians(Gourmet_spots.lat))
-            )
+    # 距離計算のSQL式（ラジアンに変換して計算。球面三角法（余弦定理）っていう計算らしい）
+    # (6371 * acos(cos(lat1) * cos(lat2) * cos(lon2 - lon1) + sin(lat1) * sin(lat2)))
+    distance_expr = (
+        EARTH_RADIUS * func.acos(
+            func.cos(func.radians(current_lat)) *
+            func.cos(func.radians(Gourmet_spots.lat)) *
+            func.cos(func.radians(Gourmet_spots.lon) - func.radians(current_lon)) +
+            func.sin(func.radians(current_lat)) *
+            func.sin(func.radians(Gourmet_spots.lat))
         )
-        
-        # クエリ定義
-        gourmet_near_spots = (
-            select(
-                Gourmet_spots.id, 
-                literal(spot_type).label('spot_type'),
-                Gourmet_spots.name, 
-                distance_expr.label('distance')
-            ).where(
-                distance_expr <= limit_km,  # 3km以内
-                distance_expr > 0
-            ).order_by(
-                distance_expr.asc() # 近い順
-            ).limit(3)
-        )
-        
-        # グルメの周辺のスポットを取得して返す
-        return db.execute(gourmet_near_spots).mappings().all()
+    )
+    
+    # クエリ定義
+    gourmet_near_spots = (
+        select(
+            Gourmet_spots.id, 
+            literal('gourmet').label('spot_type'),
+            Gourmet_spots.name, 
+            distance_expr.label('distance')
+        ).where(
+            distance_expr <= limit_km,  # 3km以内
+            distance_expr > 0
+        ).order_by(
+            distance_expr.asc() # 近い順
+        ).limit(3)
+    )
+    
+    # 観光地と周辺のSQLを合わせる
+    near_union = union_all(tourist_near_spots, gourmet_near_spots)
+    
+    # グルメの周辺のスポットを取得して返す
+    return db.execute(near_union).mappings().all()
