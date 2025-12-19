1b8d37fa5aa9ded78b7cebabdaa82a36f044a2ae
ogumakouta <siw23010010@gmail.com>
2025-12-19 13:52:17 +0900

    回答結果保存APIをmain.pyからrouters/question.pyに移動
    リファクタリング：routers/question.py

--- backend/app/routers/question.py
+++ backend/app/routers/question.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Query, Depends, HTTPException
+from fastapi import APIRouter, Query, Depends, HTTPException, status
 from typing import List, Literal
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 # データベースの接続設定をインポート
 from app.database import db_connect
 
 # データベースを操作する関数をインポート
-from app.crud.question import get_question_text
+from app.crud.question import get_question_text, save_question
 
-# レスポンスモデルをインポート
-from app.schemas.question import QestionResponse
+# レスポンスモデルとBodyの定義をインポート
+from app.schemas.question import QestionResponse, SendSaveQuestionResponse, SendSaveQuestion
 
 
 router = APIRouter()
@@ -31,3 +32,22 @@ def get_questions(
         raise HTTPException(status_code = 404, detail = "データが見つかりませんでした")
     
     return result
+
+
+# 回答結果保存
+@router.post("/questions", response_model=SendSaveQuestionResponse, status_code=201)
+def send_save_question(
+    quiz_result_data: SendSaveQuestion,
+    db: Session = Depends(db_connect)
+):
+    # スコアと問題数に不正な値が入っていた場合エラーを返す
+    if quiz_result_data.total_questions - quiz_result_data.score < 0:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail = "スコアが問題数より大きい数値ではいけません")
+    
+    try:
+        result = save_question(db, quiz_result_data.user_id, quiz_result_data.spot_type, quiz_result_data.score, quiz_result_data.total_questions)
+        
+        return result
+    
+    except IntegrityError:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = '指定されたユーザーは存在しません')
