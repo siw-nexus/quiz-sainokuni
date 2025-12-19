880264ec65c6e8f7b1ae9f8940611669d8c24b36
ogumakouta <siw23010010@gmail.com>
2025-12-19 14:12:17 +0900

    興味がある削除APIをmain.pyからrouters/interest.pyに移動
    リファクタリング：routers/inerest.py

--- backend/app/routers/interest.py
+++ backend/app/routers/interest.py
@@ -44,3 +44,21 @@ def create_interests(
         
     except IntegrityError:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = '指定されたユーザーは存在しません')
+
+
+# 興味がある削除
+@router.delete("/interests")
+def del_interest(
+    user_id: int,
+    spot_type: Literal['tourist', 'gourmet'] = Query(..., description = '観光地(tourist)かグルメ(gourmet)か'),
+    spot_id: int = Query(..., ge = 1, description = 'スポットのID（1以上の数値）'),
+    db: Session = Depends(db_connect)
+):
+    
+    result = delete_interest(db, user_id, spot_type, spot_id)
+
+    # データがからなら404エラーを返す
+    if result.rowcount == 0:
+        raise HTTPException(status_code = 404, detail = "データが見つかりませんでした")
+    
+    return {"message": "削除が完了しました"}
