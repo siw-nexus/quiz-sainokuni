48fe084e215839d55851ad003eb93a7dfb71e643
ogumakouta <siw23010010@gmail.com>
2025-12-20 19:44:51 +0900

    興味があるAPIで使うuser_idをJWT認証で取得したuser_idを使うように修正
    コード修正：routers/interest.py

--- backend/app/routers/interest.py
+++ backend/app/routers/interest.py
@@ -12,6 +12,12 @@ from app.crud.interest import get_interests, add_interests, delete_interest
 # レスポンスモデルをインポート
 from app.schemas.interest import AddInterestResponse, InterestsCreate, GetInterestResponse
 
+# JWT認証をインポート
+from app.dependencies import get_current_user
+
+# データベースのモデルをインポート
+from app.models import Users
+
 
 router = APIRouter()
 
@@ -19,10 +25,10 @@ router = APIRouter()
 # 興味がある一覧取得
 @router.get('/interests', response_model = List[GetInterestResponse])
 def get_interest(
-    user_id: int = Query(..., ge = 1, description = 'ユーザーID'),
+    current_user: Users = Depends(get_current_user),
     db: Session = Depends(db_connect)
 ):
-    result = get_interests(db, user_id)
+    result = get_interests(db, current_user.id)
     
     # データがからなら404エラーを返す
     if not result:
@@ -35,10 +41,11 @@ def get_interest(
 @router.post('/interests', response_model = AddInterestResponse, status_code=status.HTTP_201_CREATED)
 def create_interests(
     interest_data: InterestsCreate,
+    current_user: Users = Depends(get_current_user),
     db: Session = Depends(db_connect)
 ):
     try:
-        result = add_interests(db, interest_data.user_id, interest_data.spot_type, interest_data.spot_id)
+        result = add_interests(db, current_user.id, interest_data.spot_type, interest_data.spot_id)
         
         return result
         
@@ -49,13 +56,13 @@ def create_interests(
 # 興味がある削除
 @router.delete("/interests")
 def del_interest(
-    user_id: int,
+    current_user = Depends(get_current_user),
     spot_type: Literal['tourist', 'gourmet'] = Query(..., description = '観光地(tourist)かグルメ(gourmet)か'),
     spot_id: int = Query(..., ge = 1, description = 'スポットのID（1以上の数値）'),
     db: Session = Depends(db_connect)
 ):
     
-    result = delete_interest(db, user_id, spot_type, spot_id)
+    result = delete_interest(db, current_user.id, spot_type, spot_id)
 
     # データがからなら404エラーを返す
     if result.rowcount == 0:
