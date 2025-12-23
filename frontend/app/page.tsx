798ac10d9c7be2d565f0a3bf5cd7fc9352b57f45
ogumakouta <siw23010010@gmail.com>
2025-12-23 15:09:03 +0900

    アクセストークンを取得して有効かどうかを確認、その結果をQuizSelectコンポーネントに渡すコードを追加
    コード追加：app/page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,19 @@
 // コンポーネントのインポート
 import QuizSelect from "@/components/home/QuizSelect";
 
-export default function Home() {
+// 認証関係の関数をインポート
+import { getAccessToken, isTokenValid } from '@/lib/auth';
+
+export default async function Home() {
+  // アクセストークンを取得する関数を呼び出す
+  const token: string | undefined = await getAccessToken();
+
+  // アクセストークンが有効かどうかを確認する関数を呼び出す
+  const isLoggedIn: boolean = await isTokenValid(token);
+  
   return (
     <main>
-      <QuizSelect />
+      <QuizSelect isLoggedIn={isLoggedIn}/>
     </main>
   );
 }
