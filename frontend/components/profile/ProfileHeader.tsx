213d976a62e6cc1ca6e47b6d79c37f8a147ecdcf
ogumakouta <siw23010010@gmail.com>
2025-12-24 11:54:43 +0900

    Propsで受け取ったユーザー情報を表示するコードを追加
    コード追加：ProfileHeader.tsx

---
+++ frontend/components/profile/ProfileHeader.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+// 型の定義をインポート
+import { User } from "@/types/user"
+
+// Propsの定義
+type Props = {
+  user: User;
+}
+
+export default function ProfileHeader({ user }: Props) {
+  return (
+    <p>{user.name}</p>
+    <p>{user.email}</p>
+  );
+}
