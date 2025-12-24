a93e8e5fb2348281565ebc2b7504e030cd20051a
ogumakouta <siw23010010@gmail.com>
2025-12-24 15:44:37 +0900

    Propsで興味がある一覧を取得してInterestListコンポーネントに渡すコードを追加
    コード追加：ProfileScreen.tsx

--- frontend/components/profile/ProfileScreen.tsx
+++ frontend/components/profile/ProfileScreen.tsx
@@ -4,24 +4,38 @@ import { useState } from 'react';
 
 // コンポーネントをインポート
 import ProfileHeader from "./ProfileHeader"
+import InterestList from './InterestList';
 
 // 型の定義をインポート
-import { User } from "@/types/user"
+import { User } from "@/types/user";
+import { interest } from '@/types/interest';
 
 // Propsの定義
 type Props = {
   user: User;
+  interests: interest;
 }
 
-export default function ProfileScreen({ user }: Props) {
-  const [isProfile, serIsProfile]: boolean = useState(true);
+export default function ProfileScreen({ user, interests }: Props) {
+  const [isProfile, setIsProfile]: boolean = useState(false);
+  const [isInterest, setIsInterest]: boolean = useState(true);
 
-  // isProfileがtrueならProfileHeaderを表示
-  if (isProfile) {
-    return(
-      <main>
-        <ProfileHeader user={user} />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <div>
+        <button className='border'>
+          プロフィール
+        </button>
+        <button className='border'>
+          興味がある一覧
+        </button>
+      </div>
+      <div>
+        {isProfile && <ProfileHeader user={user} />}
+      </div>
+      <div>
+        {isInterest && <InterestList interests={interests}/>}
+      </div>
+    </main>
+  )
 }
