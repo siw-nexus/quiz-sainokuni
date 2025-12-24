7bc677188537f2bcd9b3e79f0c674dff5d8c203d
ogumakouta <siw23010010@gmail.com>
2025-12-24 15:43:22 +0900

    興味がある一覧を取得してProfileScreenコンポーネントに渡すコードを追加
    コード追加：profile/page.tsx

--- frontend/app/(main)/profile/page.tsx
+++ frontend/app/(main)/profile/page.tsx
@@ -3,9 +3,11 @@ import { redirect } from "next/navigation";
 // APIリクエストの関数をインポート
 import { getAccessToken, isTokenValid } from "@/lib/auth";
 import { getUser } from "@/lib/api/user";
+import { getInterest } from "@/lib/api/interest";
 
 // 型の定義をインポート
 import { User } from "@/types/user";
+import { interest } from "@/types/interest";
 
 // コンポーネントをインポート
 import ProfileScreen from "@/components/profile/ProfileScreen";
@@ -25,7 +27,10 @@ export default async function Profile() {
   // ユーザー情報を取得する関数を呼び出す
   const user: User = await getUser(token);
 
+  // 興味がある一覧を取得する関数を呼び出す
+  const interests: interest[] = await getInterest(token)
+
   return(
-    <ProfileScreen user={user} />
+    <ProfileScreen user={user} interests={interests} />
   );
 }
