d0bd41864b86d7b0bd2ac7cde58cef81cdcc85b2
ogumakouta <siw23010010@gmail.com>
2025-12-24 15:47:37 +0900

    プロフィールと興味がある一覧を切り替えるコードを追加
    コード追加：ProfileScreen.tsx

--- frontend/components/profile/ProfileScreen.tsx
+++ frontend/components/profile/ProfileScreen.tsx
@@ -20,13 +20,35 @@ export default function ProfileScreen({ user, interests }: Props) {
   const [isProfile, setIsProfile]: boolean = useState(false);
   const [isInterest, setIsInterest]: boolean = useState(true);
 
+  // プロフィールボタンを押したら実行する関数
+  const handleProfile = () => {
+    if (isProfile) return;
+
+    setIsProfile(true);
+    setIsInterest(false);
+  }
+
+  // 興味がある一覧ボタンを押したら実行する関数
+  const handleInterest = () => {
+    if (isInterest) return;
+
+    setIsProfile(false);
+    setIsInterest(true);
+  }
+
   return (
     <main>
       <div>
-        <button className='border'>
+        <button 
+          className='border'
+          onClick={handleProfile}
+        >
           プロフィール
         </button>
-        <button className='border'>
+        <button 
+          className='border'
+          onClick={handleInterest}
+        >
           興味がある一覧
         </button>
       </div>
