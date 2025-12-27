e0b219100bf7b95bb26a0203390cc984923f3c1e
ogumakouta <siw23010010@gmail.com>
2025-12-28 02:34:18 +0900

    回答履歴を保存するAPIを叩くコードを追加
    コード追加：actions/question.ts

--- frontend/actions/question.ts
+++ frontend/actions/question.ts
@@ -1,5 +1,8 @@
 'use server'
 
+// 型の定義をインポート
+import { HistoryItem } from "@/types/history";
+
 // APIのエンドポイント
 const apiUrl = process.env.INTERNAL_API_URL || 'http://backend:8000';
 
@@ -24,5 +27,36 @@ export async function saveQuestionResult(token: string, spotType: 'tourist' | 'g
   } catch (e) {
     console.error(e);
     return []; // エラーなら空配列を返す
+  }
+}
+
+export async function saveQuizHistory(token: string, resultId: number, histories: HistoryItem[]) {
+  // 回答履歴を保存
+  try {
+    const res = await fetch(`${apiUrl}/histories`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(
+        histories.map((h, index) => ({
+          quiz_result_id: Number(resultId),
+          question_num: Number(index + 1),
+          question_id: Number(h.spot_id),
+          choice_id: Number(h.userAnswerId),
+          is_correct: Boolean(h.isCorrect)
+        }))
+      )
+    });
+    
+    // レスポンスの確認
+    if (!res.ok) throw new Error("回答履歴の保存に失敗しました");
+
+    // 結果を返す
+    return res.json();
+
+  } catch (e) {
+    console.error(e);
   }
 }
